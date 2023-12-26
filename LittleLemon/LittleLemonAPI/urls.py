aa410b6046fe416bcd06dbb8f314d90e94f17546
Radicitus <novivolatus@gmail.com>
2023-12-26 14:44:22 -0800

    Add adding/removing/viewing users from delivery crew

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('groups/manager/users', views.group_management_managers),
     path('groups/manager/users/<int:user_id>', views.group_management_managers),
+    path('groups/delivery-crew/users', views.group_management_delivery_crew),
+    path('groups/delivery-crew/users/<int:user_id>', views.group_management_delivery_crew),
 ]
 
--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -38,7 +38,7 @@ def group_management_managers(request, user_id=None):
 
     if request.method == 'GET':
         manager_names = list(managers.user_set.values_list('username', flat=True))
-        return Response(manager_names, 200)
+        return Response(manager_names, status.HTTP_200_OK)
 
     if request.method == 'POST':
         username = request.data['username']
@@ -56,5 +56,34 @@ def group_management_managers(request, user_id=None):
             managers.user_set.remove(user)
 
             return Response(f'User with id of {user_id} removed from Managers successfully.', status.HTTP_200_OK)
+
+        return Response("No user ID provided.", status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsManager])
+def group_management_delivery_crew(request, user_id=None):
+    delivery_crew = Group.objects.get(name="Delivery Crew")
+
+    if request.method == 'GET':
+        delivery_crew_names = list(delivery_crew.user_set.values_list('username', flat=True))
+        return Response(delivery_crew_names, status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        username = request.data['username']
+        if username:
+            user = get_object_or_404(User, username=username)
+            delivery_crew.user_set.add(user)
+
+            return Response(f'User {username} added to Delivery Crew successfully.', status.HTTP_201_CREATED)
+
+        return Response("No username provided.", status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        if user_id is not None:
+            user = get_object_or_404(User, id=user_id)
+            delivery_crew.user_set.remove(user)
+
+            return Response(f'User with id of {user_id} removed from Delivery Crew successfully.', status.HTTP_200_OK)
 
         return Response("No user ID provided.", status.HTTP_400_BAD_REQUEST)
