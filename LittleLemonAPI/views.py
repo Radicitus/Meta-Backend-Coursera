caad5a4122860424593922b3c0a962c207e11574
Radicitus <novivolatus@gmail.com>
2023-12-26 11:44:27 -0800

    Ensure customer and delivery can only access safe requests and manager can do all

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,13 +1,26 @@
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        else:
+            return [IsAdminUser()]
+
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        else:
+            return [IsAdminUser()]
