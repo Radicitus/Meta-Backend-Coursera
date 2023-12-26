648ec6ae0e3ce97f8a203e22b072127cf2869ece
Radicitus <novivolatus@gmail.com>
2023-12-26 12:07:15 -0800

    Move git repo 2

---
+++ LittleLemon/LittleLemonAPI/views.py
@@ -0,0 +1,26 @@
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, DjangoModelPermissions
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        else:
+            return [DjangoModelPermissions()]
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        else:
+            return [DjangoModelPermissions()]
