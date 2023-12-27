14f716e9eb298db03bca310f5698ce392282245f
Radicitus <novivolatus@gmail.com>
2023-12-26 19:53:07 -0800

    Added emptying cart

--- LittleLemon/LittleLemonAPI/views/CartViews.py
+++ LittleLemon/LittleLemonAPI/views/CartViews.py
@@ -1,14 +1,20 @@
+from rest_framework.response import Response
 from LittleLemonAPI.models import Cart
 from LittleLemonAPI.permissions import IsCustomer
 from LittleLemonAPI.serializers import CartSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 
 
-class CartView(generics.ListCreateAPIView, generics.DestroyAPIView):
+class CartView(generics.ListCreateAPIView):
     serializer_class = CartSerializer
 
     def get_queryset(self):
         return Cart.objects.filter(user=self.request.user)
+
+    def delete(self, request):
+        cart = Cart.objects.filter(user=self.request.user)
+        cart.delete()
+        return Response("Cart successfully deleted.", status.HTTP_200_OK)
 
     def get_permissions(self):
         return [IsCustomer()]
