cb2f87ff6b0ff4d3942337b58f52d02355ab54c6
Radicitus <novivolatus@gmail.com>
2023-12-26 18:35:16 -0800

    Organize app urls

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import GroupManagementViews, MenuItemViews
+from .views import GroupManagementViews, MenuItemViews, CartViews
 
 urlpatterns = [
+    # Menu Items
     path('menu-items', MenuItemViews.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', MenuItemViews.SingleMenuItemView.as_view()),
+    # Group Management
     path('groups/manager/users', GroupManagementViews.group_management_managers),
     path('groups/manager/users/<int:user_id>', GroupManagementViews.group_management_managers),
     path('groups/delivery-crew/users', GroupManagementViews.group_management_delivery_crew),
     path('groups/delivery-crew/users/<int:user_id>', GroupManagementViews.group_management_delivery_crew),
+    # Cart
+    path('cart/menu-items', CartViews.CartView.as_view())
 ]
 
