3d862d1e0e76165b9d54b7fc3066c4a4a4a24c4e
Radicitus <novivolatus@gmail.com>
2023-12-26 18:35:33 -0800

    Update models

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.title
+
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
@@ -21,8 +24,6 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
         unique_together = ('menu_item', 'user')
@@ -31,7 +32,7 @@ class Cart(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
-    status = models.BooleanField(db_index=True, default=0)
+    status = models.BooleanField(db_index=True, default=False)
     total = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(db_index=True)
 
