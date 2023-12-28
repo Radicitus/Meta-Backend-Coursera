d11cd53475c8050dfa913961cddff812104dc38d
Radicitus <novivolatus@gmail.com>
2023-12-27 21:55:38 -0800

    Add setting order status and delivery crew

--- LittleLemon/LittleLemonAPI/views/OrderViews.py
+++ LittleLemon/LittleLemonAPI/views/OrderViews.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
@@ -5,17 +6,17 @@ from LittleLemonAPI.models import Order, OrderItem
 from LittleLemonAPI.permissions import IsCustomer, IsManager, IsDeliveryCrew
 from LittleLemonAPI.serializers import OrderSerializer, OrderItemSerializer
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class OrdersView(generics.ListCreateAPIView):
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        if IsCustomer.check(self.request):
-            return Order.objects.filter(user=self.request.user)
         if IsManager.check(self.request):
             return Order.objects.all()
+        if IsCustomer.check(self.request):
+            return Order.objects.filter(user=self.request.user)
         if IsDeliveryCrew.check(self.request):
             return Order.objects.filter(delivery_crew=self.request.user)
 
@@ -25,15 +26,45 @@ class OrdersView(generics.ListCreateAPIView):
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 @permission_classes([IsAuthenticated])
-def SingleOrderView(request, order_id=None):
-    if request.method == 'GET':
-        if IsCustomer.check(request) and order_id is not None:
-            order = Order.objects.get(id=order_id)
+def SingleOrderView(request, order_id):
+    order = Order.objects.get(id=order_id)
 
-            if order.user == request.user:
-                order_items = OrderItem.objects.filter(order=order)
-                order_items_serializer = OrderItemSerializer(order_items, many=True)
-                order_items_serializer_data = order_items_serializer.data
-                return Response(order_items_serializer_data, status.HTTP_200_OK)
+    if request.method == 'GET':
+        if order.user == request.user or IsManager.check(request):
+            order_items = OrderItem.objects.filter(order=order)
+            order_items_serializer = OrderItemSerializer(order_items, many=True)
+            order_items_serializer_data = order_items_serializer.data
+            return Response(order_items_serializer_data, status.HTTP_200_OK)
+        else:
+            return Response("You are not authorized to view this order.", status.HTTP_401_UNAUTHORIZED)
+
+    if request.method == 'PATCH':
+        if IsDeliveryCrew.check(request) or IsManager.check(request):
+            order_status = request.data.get('status')
+            if order_status:
+                order.status = order_status
+                order.save()
+
+                return Response("Order status has been updated.", status.HTTP_200_OK)
             else:
-                return Response("You are not authorized to view this order.", status.HTTP_401_UNAUTHORIZED)
+                return Response("Missing status parameter.", status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PUT':
+        if IsManager.check(request):
+            delivery_crew_name = request.data.get('delivery_crew')
+
+            if delivery_crew_name:
+                try:
+                    delivery_crew = User.objects.get(username=delivery_crew_name)
+                except User.DoesNotExist:
+                    return Response("Delivery crew does not exist.", status.HTTP_400_BAD_REQUEST)
+
+                order.delivery_crew = delivery_crew
+                order.save()
+
+                return Response(f"Order delivery crew has been updated to {delivery_crew_name}.", status.HTTP_200_OK)
+
+            return Response("Missing delivery crew name parameter.", status.HTTP_400_BAD_REQUEST)
+
+        else:
+            return Response("You are not authorized to update this order.", status.HTTP_401_UNAUTHORIZED)
