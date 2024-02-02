db417978b6701c72a426c548cd261ae883bc9c9c
Radicitus <novivolatus@gmail.com>
2024-02-01 21:44:18 -0500

    Restructure files 2

--- LittleLemon/LittleLemonAPI/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem, Cart, Order, OrderItem, Category
-
-# Register your models here.
-admin.site.register(MenuItem)
-admin.site.register(Cart)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Category)
